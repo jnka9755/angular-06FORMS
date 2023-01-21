ab653a45b843297fd41ca9fb53e771788d8cf9a0
Juan Santana <juanka9755@hotmail.com>
2023-01-21 10:53:29 -0500

    06-FORMS: Formularios reactivos
    Se implementa lógica formulario de switchs

--- src/app/reactive/switchs/switchs.component.ts
+++ src/app/reactive/switchs/switchs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switchs',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchsComponent implements OnInit {
 
-  constructor() { }
+  public personForm: FormGroup = this.formBuilder.group({
+    gender: [ 'M', [ Validators.required ] ],
+    notifications: [ true , [ Validators.required ]],
+    termsConditions: [false, [ Validators.required, Validators.requiredTrue ]]
+  });
 
-  ngOnInit(): void {
+  public person = {
+    gender: 'F',
+    notifications: true
   }
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.personForm.reset({
+      ...this.person,
+      termsConditions: false
+    });
+
+    // this.personForm.get('termsConditions')?.valueChanges.subscribe( value => {
+    //   console.log(value);
+    // });
+
+    this.personForm.valueChanges.subscribe( ({termsConditions, ...rest }) => {
+
+      this.person = rest;
+    });
+  }
+
+  save(){
+    const formValue = { ...this.personForm.value };
+    delete formValue.termsConditions;
+
+    this.person = formValue;
+  }
 }
